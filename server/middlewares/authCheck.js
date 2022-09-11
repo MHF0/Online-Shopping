c1d3be03a684c72206843d52c83835920a5d6ca9
Mohammed Hesham Farhan <moh.hesham.f@gmail.com>
2022-09-11 20:07:30 +0300

    Create authCheck.js

---
+++ server/middlewares/authCheck.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+const authCheck = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).json({
+      message: "Login required",
+    });
+  }
+  const token = req.headers.authorization.split(" ").pop();
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({
+          message: "Invalid token",
+        });
+      } else {
+        req.user = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.status(401).json({
+      message: "No token provided",
+    });
+  }
+};
+
+module.exports = authCheck;
