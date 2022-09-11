bcc4884df8b6206a5b9d9241bb376fda147ddf98
Mohammed Hesham Farhan <moh.hesham.f@gmail.com>
2022-09-11 21:05:20 +0300

    Crate Comment

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -3,7 +3,7 @@ const ProductSchema = require("../database/modules/productSchema");
 const createProduct = async (req, res) => {
   try {
     const { name, price, description, image, category } = req.body;
-    const userId = req.user.userId;
+    const userId = req.user.id;
     const product = await ProductSchema.findOne({ name });
     if (product) {
       return res.status(400).json({ msg: "Product already exists." });
@@ -53,8 +53,30 @@ const deleteProduct = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
   try {
-    const products = await ProductSchema.find();
+    const products = await ProductSchema.find()
+      .populate("whoCreated", "name")
+      .populate("comments.whoCommented", "name");
     res.status(200).json({ products });
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+const createComment = async (req, res) => {
+  try {
+    const { comment } = req.body;
+    const userId = req.user.id;
+    const product = await ProductSchema.findById(req.params.id);
+    if (!product) {
+      return res.status(400).json({ msg: "Product does not exist." });
+    }
+    const newComment = {
+      comment,
+      whoCommented: userId,
+    };
+    product.comments.push(newComment);
+    await product.save();
+    res.status(200).json({ msg: "Comment created successfully." });
   } catch (err) {
     return res.status(500).json({ msg: err.message });
   }
@@ -65,4 +87,5 @@ module.exports = {
   updateProduct,
   deleteProduct,
   getAllProducts,
+  createComment,
 };
--- server/routes/product.js
+++ server/routes/product.js
@@ -7,6 +7,7 @@ const {
   updateProduct,
   deleteProduct,
   getAllProducts,
+  createComment,
 } = require("../controllers/product");
 const authCheck = require("../middlewares/authCheck");
 
@@ -14,5 +15,6 @@ productRouter.post("/create", authCheck, createProduct);
 productRouter.put("/update/:id", authCheck, updateProduct);
 productRouter.delete("/delete/:id", authCheck, deleteProduct);
 productRouter.get("/all", getAllProducts);
+productRouter.post("/comment/:id", authCheck, createComment);
 
 module.exports = productRouter;
